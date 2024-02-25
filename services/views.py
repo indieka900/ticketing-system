d2a3f1d36fd63a513c5ccb48e22ce8980b78de6b
Joseph <indiekaj@gmail.com>
2024-02-25 23:22:02 +0300

    fixes

--- services/views.py
+++ services/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from services.models import (Complaint,Feedback,Department)
 
 @login_required
 def home(request):
     
-    context = {}
+    complaints = Complaint.objects.filter(sender = request.user)
+    
+    if request.method == 'POST':
+        if 'solved' in request.POST:
+            id = request.POST.get('id')
+            complaint = Complaint.objects.get(pk=id)
+            complaint.status = 'Solved'
+            complaint.save()
+            #success message
+    
+    context = {
+        'complaints':complaints
+    }
     
     return render(request, 'app/index.html', context)
 
