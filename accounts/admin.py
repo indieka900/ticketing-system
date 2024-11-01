301e7a3379d1b78bbf94f4c640c9dbfae1c5f9f5
Joseph <indiekaj@gmail.com>
2024-11-02 01:30:04 +0300

    added groups to the custom user config

--- accounts/admin.py
+++ accounts/admin.py
@@ -19,19 +19,29 @@ def set_chairpersons(modeladmin, request, queryset):
 @admin.register(MyUser)
 class UserAdminConfig(UserAdmin):
     ordering = ('-date_joined',)
-    search_fields = ('email','full_name','username')
-    list_filter = ('email','is_active','is_staff','role',)
-    list_display = ('id','email','full_name','phone','reg_no','username','role','is_active')
+    search_fields = ('email', 'full_name', 'username')
+    list_filter = ('email', 'is_active', 'is_staff', 'role')
+    list_display = ('id', 'email', 'full_name', 'phone', 'reg_no', 'username', 'role', 'is_active', 'get_groups')
+    list_display_links = ('email', 'username')
+
     fieldsets = (
-        (None, {'fields': ('email','password','full_name','phone','username','reg_no',)}),
-        ("Permissions", {"fields" :('is_staff','is_active','is_superuser',)}),
-        ("Personal", {"fields":("role","profile_pic")}),
+        (None, {'fields': ('email', 'password', 'full_name', 'phone', 'username', 'reg_no')}),
+        ("Permissions", {"fields": ('is_staff', 'is_active', 'is_superuser', 'groups')}),
+        ("Personal", {"fields": ("role", "profile_pic")}),
     )
+
     add_fieldsets = (
         (None, {
-            'classes':('wide',),
-            'fields': ('email','full_name','reg_no','password1','password2','phone','is_staff','is_active','is_superuser','profile_pic')
+            'classes': ('wide',),
+            'fields': ('email', 'full_name', 'reg_no', 'password1', 'password2', 'phone', 'is_staff', 'is_active', 'is_superuser', 'profile_pic', 'groups'),
         }),
     )
+
+    @admin.display(description='Teams')
+    def get_groups(self, obj):
+        # Display groups as a comma-separated string
+        groups = obj.groups.all()
+        return ", ".join([group.name for group in groups]) if groups else "No Teams"
+
     actions = [activate_users,deactivate_users,set_chairpersons]
 # Register your models here.
